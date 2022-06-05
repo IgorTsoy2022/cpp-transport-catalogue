b8c107039354f064c8cb949cf64d7f2647f06bf7
IgorTsoy2022 <97473595+IgorTsoy2022@users.noreply.github.com>
2022-06-05 15:55:45 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,18 @@
-// место для вашего кода
+#include "transport_catalogue.h"
+#include "input_reader.h"
+#include "stat_reader.h"
+
+//#include <sstream>
+#include <fstream>
+
+using namespace std::string_literals;
+using namespace std::string_view_literals;
+
+int main() {
+
+    cat::TransportCatalogue tc;
+    auto queries = cat::queries::QueriesToDataBase(tc);   
+    cat::print::Info(tc, queries, 6);
+
+    return 0;
+}
